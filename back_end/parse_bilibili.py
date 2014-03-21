0f3e5f318db118498c819e82a0e6d2f63a72c4f5
RicterZ <RicterZheng@gmail.com>
2014-03-21 23:21:21 +0800

    add parse_bilibili.py

---
+++ back_end/parse_bilibili.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+"""
+这里的都是解析bilibili各种API的函数
+"""
+
+
+def parse_epi(sp_id, season_id=0):
+    # sp_id 就是动漫的专辑id，season_id 是季数，此函数应自行判断然后生成 url 获取集数。
+    # 注意：要用最短的最优雅的代码来实现
+    pass
