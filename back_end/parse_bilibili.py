ec57e9fac47d2e1b698b34e4042f76f5fda9d375
Madimo Zhang <MadimoZhang@gmail.com>
2014-03-22 00:14:49 +0800

    finish parse_epi at #4

--- back_end/parse_bilibili.py
+++ back_end/parse_bilibili.py
@@ -1,11 +1,25 @@
 #coding:utf-8
 
+import urllib2
+
 """
 这里的都是解析bilibili各种API的函数
 """
 
-
 def parse_epi(sp_id, season_id=0):
-    # sp_id 就是动漫的专辑id，season_id 是季数，此函数应自行判断然后生成 url 获取集数。
-    # 注意：要用最短的最优雅的代码来实现
-    pass
+    
+    param = str(sp_id)
+    if season_id:
+    	param += '-' + str(season_id)
+
+    request_url = 'http://www.bilibili.tv/sppage/bangumi-%s-1.html' % param
+
+    result = urllib2.urlopen(request_url).read()
+    begin  = result.find('<div class="t">')
+    end    = result.find('</div></a>')
+
+    if begin == -1 or end == -1:
+    	return 0
+
+    return int(result[begin+18 : end-3])
+
