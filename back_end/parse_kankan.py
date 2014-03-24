c13fb4763df4f000f2d8481028e5a1b67e7ecd27
Madimo Zhang <MadimoZhang@gmail.com>
2014-03-24 20:49:40 +0800

    finish get_kankan_anime_detail at #5

--- back_end/parse_kankan.py
+++ back_end/parse_kankan.py
@@ -1,19 +1,38 @@
 #coding: utf-8
 from django.utils.timezone import now
+import urllib2
+import json
 
 
 def get_kankan_anime_detail(aid):
     """
     参考:
-    http://data.pad.kankan.com/mobile/detail/74/74030.json
-    http://data.pad.kankan.com/mobile/sub_detail/74/74030.json
+    http://data.pad.kankan.com/mobile/detail/74/74031.json
+    http://data.pad.kankan.com/mobile/sub_detail/74/74031.json
     """
-    #TODO
+
+    str_aid = str(aid)
+    aid_url = str_aid[0] + str_aid[1] + '/' + str_aid + '.json'
+    detail_url = 'http://data.pad.kankan.com/mobile/detail/' + aid_url
+    sdetail_url = 'http://data.pad.kankan.com/mobile/sub_detail/' + aid_url
+
+    result = urllib2.urlopen(detail_url).read()
+    jobject = json.loads(result)
+    name = jobject['title']
+    intro = jobject['intro']
+    poster_link = jobject['poster']
+    is_end = False
+    if jobject['episodeCount'] == jobject['totalEpisodeCount']:
+        is_end = True
+
+    #result = urllib2.urlopen(sdetail_url).read()
+    #jobject = json.loads(result)
+
     return {
         'aid': aid,
-        'name': '轻音少女',
-        'intro': '介绍!!!!',
-        'is_end': True,
+        'name': name,
+        'intro': intro,
+        'is_end': is_end,
 
         # 这里默认先不实现
         'bilibili_aid': 0,
@@ -21,6 +40,7 @@ def get_kankan_anime_detail(aid):
         'bilibili_bgmcount': 0,
         'bilibili_season': 1,
 
-        'poster_link': '/xx/xx/xx.jpg',
+        'poster_link': poster_link,
         'updated_time': now()
     }
+
