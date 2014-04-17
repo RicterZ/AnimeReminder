c34437f6232e90ff8ea462d383e36097147955c1
RicterZ <RicterZheng@gmail.com>
2014-04-17 11:25:33 +0800

    fix bug

--- lib/schedule.py
+++ lib/schedule.py
@@ -1,38 +1,31 @@
-from settings import *
+import re, json
+from anime import AnimeDataGetter
 
 anime = AnimeDataGetter()
 url = 'http://anime.kankan.com/'
-reqData = anime.getURL(url)
-animeList = []
-updateList = re.compile(r'updateList_week_data\[[\d]+\] = \{(.*)\}')
-for updateItem in updateList.findall(reqData):
+req_data = anime.getURL(url)
+
+anime_list = []
+updateList = re.compile('updatelist_week_data\[\d+\] = \{(.*)\};')
+for updateItem in updateList.findall(req_data):
     updateJson = json.loads('{'+updateItem+'}')
-    animeList.append({
+    anime_list.append({
         "week": updateJson["day_id"],
         "time": updateJson["schedule"].split(' ')[1],
         "url": updateJson["link"],
         "name": updateJson["title"],
     })
+db.update('schedule', where='id=1', raw_data=json.dumps(anime_list))
 
-db.update('schedule', where='id=1', raw_data=json.dumps(animeList))
-
-req_data = anime.getURL(url)
 anime_list = []
-
-update_list = re.compile(r'<li>\d\d:\d\d.*<a.*')
-update_time = re.compile(r'<ul id="sche_show_\d">')
-time = re.compile(r'\d\d:\d\d')
-href = re.compile(r'http://.*\d\d\d\d\d')
-name = re.compile(r'">.*</a>')
-for i in req_data.split('\n'):
-    if update_time.findall(i):
-        weekday = update_time.findall(i)[0].split('"')[1].split('_')[2]
-        data = update_list.findall(i)[0].decode('utf8')
-        anime_list.append({
-            "week": weekday,
-            "time": time.findall(data)[0],
-            "url": href.findall(data)[0],
-            "name": name.findall(data)[0].strip('">').split('<')[0]
-        })
-
-db.update('schedule', where='id=2', raw_data=json.dumps(animeList))
+update_list = re.compile('<ul id="sche_show_(\d)">([\w\W]+?)</ul>')
+update_data = re.compile('<li>(\d{2}\:\d{2}).*<a  href="(.*)" blockid="\d\d\d\d">(.*)</a></li>')
+for i in update_list.findall(req_data):
+    for data in update_data.findall(i[1]):
+        anime_list.append(
+            "week": i[0],
+            "time": data[0],
+            "url": data[1],
+            "name": data[2],
+        )
+db.update('schedule', where='id=2', raw_data=json.dumps(anime_list))
