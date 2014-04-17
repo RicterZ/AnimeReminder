f2722773c3e0e5ac0a17154db5e5fdebf793c37d
RicterZ <RicterZheng@gmail.com>
2014-04-17 11:43:48 +0800

    fix bug

--- lib/handlers/common/ScheduleHandler.py
+++ lib/handlers/common/ScheduleHandler.py
@@ -5,12 +5,12 @@ from lib.settings import *
 class ScheduleGetHandler(object):
     def GET(self):
         web.header('Content-type', "application/json; charset=utf-8")
-        anime_list = db.select('schedule', where='id=2')
-        return returnData(data={"update_list": json.loads(anime_list)})
+        anime_list = db.select('schedule', where='id=2')[0]
+        return returnData(data={"update_list": json.loads(anime_list.raw_data)})
 
 
 class ScheduleGetHandlerV2(object):
     def GET(self):
         web.header('Content-type', "application/json; charset=utf-8")
-        anime_list = db.select('schedule', where='id=1')
-        return returnData(data={"update_list": json.loads(anime_list)})
+        anime_list = db.select('schedule', where='id=1')[0]
+        return returnData(data={"update_list": json.loads(anime_list.raw_data)})
