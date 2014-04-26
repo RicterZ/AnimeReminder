dd8bb2768cd9dc35ad81f244cf188693a5d83022
RicterZ <RicterZheng@gmail.com>
2014-04-26 19:59:49 +0800

    revert #9

--- back_end/tests.py
+++ back_end/tests.py
@@ -8,7 +8,7 @@ class ParserTest(SimpleTestCase):
     def test_parse_bilibili(self):
         data = get_bilibili_anime_detail('日常')
         self.assertEqual(data['bilibili_aid'], 782)
-        self.assertEqual(data['bilibili_bgmcount'], 26)
+        self.assertEqual(data['bilibili_bgmcount'], 28)
         self.assertEqual(data['bilibili_season'], 1)
 
         data = get_bilibili_anime_detail('中二病也要谈恋爱！')
@@ -22,11 +22,13 @@ class ParserTest(SimpleTestCase):
         self.assertEqual(data['bilibili_season'], 2)
 
         data = get_bilibili_anime_detail('魔法少女小圆')
-        self.assertEqual(data['bilibili_bgmcount'], 12)
+        self.assertEqual(data['bilibili_bgmcount'], 13)
 
         data = get_bilibili_anime_detail('幸运星')
+        print data
         self.assertEqual(data['bilibili_bgmcount'], 24)
 
     def test_search_anime(self):
         data = search_anime('魔法少女小圆')
+        print data
         self.assertEqual([('魔法少女小圆', '60075'), ('魔法少女小圆 剧场版', '73391')], data)
